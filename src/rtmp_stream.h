10e58dcd2400ef5f0f7464a7fdbadfff1f84c4d9
Olivier Cervello <ocervello@google.com>
2019-10-05 19:34:49 +0200

    Update

--- src/rtmp_stream.h
+++ src/rtmp_stream.h
@@ -1,17 +1,19 @@
-#pragma once
 #ifndef RTMP_STREAM_H
 #define RTMP_STREAM_H
+#include "darknet.h"
 
-// See https://stackoverflow.com/questions/57183443/linkage-error-when-including-npps-h-from-cuda-10-0
 #ifdef __cplusplus
 extern "C" {
 #endif
 #include "image.h"
 
 void init_rtmp_server(double width, double height, int fps, int bitrate, char *codec_profile, char *server);
+
+#ifdef OPENCV
 void send_rtmp_frame(mat_cv* mat);
 
-// See https://stackoverflow.com/questions/57183443/linkage-error-when-including-npps-h-from-cuda-10-0
+#endif  // OPENCV
+
 #ifdef __cplusplus
 }
 #endif

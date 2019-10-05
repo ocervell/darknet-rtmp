645f0931eccd766ce3c5616b6c939f8696fe35d5
Olivier Cervello <ocervello@google.com>
2019-10-05 18:27:50 +0200

    Fix CUDA error

--- src/rtmp_stream.h
+++ src/rtmp_stream.h
@@ -2,16 +2,18 @@
 #ifndef RTMP_STREAM_H
 #define RTMP_STREAM_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+// See https://stackoverflow.com/questions/57183443/linkage-error-when-including-npps-h-from-cuda-10-0
+// #ifdef __cplusplus
+//extern "C" {
+//#endif
 #include "image.h"
 
 void init_rtmp_server(double width, double height, int fps, int bitrate, char *codec_profile, char *server);
 void send_rtmp_frame(IplImage* ipl);
 
-#ifdef __cplusplus
-}
-#endif
+// See https://stackoverflow.com/questions/57183443/linkage-error-when-including-npps-h-from-cuda-10-0
+//#ifdef __cplusplus
+//}
+//#endif
 
 #endif // RTMP_STREAM_H
